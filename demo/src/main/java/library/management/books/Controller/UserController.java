68316589279ad2e7768d3b2f5dfae94747a15ab8
kunal-pandey12 <kunalpandey.12dev@gmail.com>
2026-03-29 00:27:06 +0530

    updated User service logic

--- demo/src/main/java/library/management/books/Controller/UserController.java
+++ demo/src/main/java/library/management/books/Controller/UserController.java
@@ -4,10 +4,7 @@ import library.management.books.Dto.UserDto;
 import library.management.books.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,22 @@ public class UserController {
     public List<UserDto> createUser(@RequestBody List<UserDto> userDto){
         return userService.createUser(userDto);
     }
+    @GetMapping("/getAll")
+    public List<UserDto>getAllUser(){
+        return userService.getAll();
+    }
+    @GetMapping("/{id}")
+    public UserDto getById(@PathVariable Long id){
+        return userService.getByUserId(id);
+    }
 
+    @PutMapping("/{id}")
+    public UserDto getById(@PathVariable Long id, @RequestBody UserDto userDto ){
+        return userService.updateUser(id, userDto);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return "Delete successfully user id ";
+    }
 }
--- demo/src/main/java/library/management/books/Service/UserService.java
+++ demo/src/main/java/library/management/books/Service/UserService.java
@@ -1,13 +1,10 @@
 package library.management.books.Service;
-
 import library.management.books.Dto.UserDto;
 import library.management.books.Entity.Role;
 import library.management.books.Entity.UserEntity;
 import library.management.books.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -37,5 +34,58 @@ public class UserService {
 
             return response;
         }).toList();
+    }
+    public List<UserDto>getAll(){
+        List<UserEntity>users=userRepo.findAll();
+
+        return users.stream().map(user -> {
+            UserDto dto=new UserDto();
+            dto.setId(user.getId());
+            dto.setName(user.getName());
+            dto.setEmail(user.getEmail());
+            dto.setRole(user.getEmail());
+            dto.setPassword(user.getPassword());
+            return dto;
+        }).toList();
+
+        }
+
+        public UserDto getByUserId(Long id){
+        UserEntity userEntity=userRepo.findById(id)
+                .orElseThrow(()-> new RuntimeException("User id not found"));
+
+        UserDto dto=new UserDto();
+        dto.setId(userEntity.getId());
+        dto.setName(userEntity.getName());
+        dto.setEmail(userEntity.getEmail());
+        dto.setRole(userEntity.getEmail());
+        dto.setPassword(userEntity.getPassword());
+        return dto;
+        }
+
+    public UserDto updateUser(Long id,UserDto userDto){
+        UserEntity userEntity=userRepo.findById(id)
+                .orElseThrow(()->new RuntimeException("User Id not found"));
+
+        userEntity.setName(userDto.getName());
+        userEntity.setEmail(userDto.getEmail());
+        userEntity.setRole(Role.valueOf(userDto.getRole().toUpperCase()));
+        userEntity.setPassword(userDto.getPassword());
+
+        UserEntity updateUser=userRepo.save(userEntity);
+
+        UserDto dto=new UserDto();
+        dto.setId(updateUser.getId());
+        dto.setName(updateUser.getName());
+        dto.setEmail(updateUser.getEmail());
+        dto.setRole(updateUser.getRole().name());
+        dto.setPassword(updateUser.getPassword());
+
+        return dto;
+    }
+    public void deleteUser(Long id){
+        UserEntity userEntity=userRepo.findById(id)
+                .orElseThrow(()->new RuntimeException("User id not found"));
+        userRepo.delete(userEntity);
     }
 }
