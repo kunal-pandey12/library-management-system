613c88a497ec500a69743163fda0711bcfdcc802
kunal-pandey12 <kunalpandey.12dev@gmail.com>
2026-04-15 00:35:59 +0530

    Added issue-return system with exception handling

---
+++ demo/src/main/java/library/management/books/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package library.management.books.Exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+
+
+    }
+}
