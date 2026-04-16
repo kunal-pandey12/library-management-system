738319e8f36983a36e379dec320d82130ac38e84
kunal-pandey12 <kunalpandey.12dev@gmail.com>
2026-04-17 00:25:06 +0530

    git add IssueRepo.java git commit -m "Add duplicate issue validation method in IssueRepo"

--- demo/src/main/java/library/management/books/Repo/IssueRepo.java
+++ demo/src/main/java/library/management/books/Repo/IssueRepo.java
@@ -1,8 +1,12 @@
 package library.management.books.Repo;
 
+import library.management.books.Entity.BookEntity;
 import library.management.books.Entity.IssueEntity;
+import library.management.books.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IssueRepo extends JpaRepository<IssueEntity,Long> {
+
+    boolean existsByUserAndBookAndReturnDateIsNull(UserEntity user, BookEntity book);
 
 }
